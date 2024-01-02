438e7acb6016207a5b24ee7923142874de16b8cf
vuquyen03 <quyenvungoc1711@gmail.com>
2024-01-02 10:25:58 +0700

    .

--- preprocessing.py
+++ preprocessing.py
@@ -9,12 +9,6 @@ minsize = 20
 threshold = [0.6, 0.7, 0.7]
 factor = 0.709
 
-def to_rgb (img):
-    w, h = img.shape
-    ret = np.empty((w,h,3), dtype=np.uint8)
-    ret[:,:,0] = ret[:,:,1]=ret[:,:,2]=img
-    return ret
-
 print ("Creating networks and loading parameters")
 gpu_memory_fraction = 1.0
 with tf.Graph().as_default():
@@ -33,34 +27,30 @@ with tf.Graph().as_default():
         pnet, rnet, onet = detect_face.create_mtcnn(sess, None)
 
 
-  
-# video_capture = cv2.VideoCapture(0)
-# while True:
-#     # Capture frame by frame
-#     ret, frame = video_capture.read()
-    
-#     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    
-#     if gray.ndim == 2:
-#         img = to_rgb(gray)
-        
-#     bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
-#     nrof_faces = bounding_boxes.shape[0]
-    
-#     for face_postion in bounding_boxes:
-#         face_postion = face_postion.astype(int)
-        
-#         cv2.rectangle(frame,
-#                       (face_postion[0], face_postion[1]),
-#                       (face_postion[2], face_postion[3]),
-#                       (0,255,0), 2)
-        
-#     cv2.imshow('MTCNN Demo', frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+def camera_detect():
+    video_capture = cv2.VideoCapture(0)
+    while True:
+        # Capture frame by frame
+        ret, frame = video_capture.read()
 
-# video_capture.release()
-# cv2.destroyAllWindows()
+        img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+        nrof_faces = bounding_boxes.shape[0]
+
+        for face_postion in bounding_boxes:
+            face_postion = face_postion.astype(int)
+
+            cv2.rectangle(frame,
+                          (face_postion[0], face_postion[1]),
+                          (face_postion[2], face_postion[3]),
+                          (0,255,0), 2)
+
+        cv2.imshow('MTCNN Demo', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 def detectFace(input, output, newSize):
@@ -73,7 +63,7 @@ def detectFace(input, output, newSize):
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         
         bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
-        nrof_faces = bounding_boxes.shape[0]
+        # nrof_faces = bounding_boxes.shape[0]
         
         for i, face_position in enumerate(bounding_boxes):
             face_position = face_position.astype(int)
@@ -86,7 +76,8 @@ def detectFace(input, output, newSize):
             # chỉ lấy 1 mặt ở trung tâm bức ảnh
             break
 
-input = 'lfw/hieu'
+input = 'hieu/'
 output = 'test/'
-detectFace(input, output, 100)
+# detectFace(input, output, 100)
+camera_detect()
 
